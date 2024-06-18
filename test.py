8865ea2aa989804676f202ae73f840e5e82a4d6c
jkrenick2022 <130420912+jkrenick2022@users.noreply.github.com>
2024-06-17 22:32:14 -0400

    Updated research logic

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ from supabase import create_client, Client
 import statsapi
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
+import io
 
 load_dotenv()
 
@@ -21,19 +22,32 @@ SUPABASE_KEY = os.getenv('SUPABASE_KEY')
 # Initialize Supabase client
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
+# Initialize the bot with commands and intents
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+intents.message_content = True  # Ensure the bot can read message content
 
-def convert_to_est(timestamp: str) -> datetime:
+prefixes = ['mlb ', 'MLB ']
+bot = commands.Bot(command_prefix=['mlb ', 'MLB '],
+                   intents=intents, help_command=None)
 
-    # Remove the 'Z' character and parse the datetime string into a naive datetime object
-    dt = datetime.strptime(timestamp.rstrip('Z'), '%Y-%m-%dT%H:%M:%S')
+# Initializes the bot when it is logged on
 
-    # Convert the naive datetime object from UTC to Eastern Time
-    utc = pytz.timezone('UTC')
-    dt_utc = utc.localize(dt)
-    eastern = pytz.timezone('US/Eastern')
-    dt_eastern = dt_utc.astimezone(eastern)
 
-    return dt_eastern
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+    activity = discord.Game(name="MLB Help")
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+
+
+def convert_to_12hr_format(timestamp: str) -> str:
+    # Parse the datetime string into a naive datetime object
+    dt = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S')
+
+    # Format the datetime object to a string in 12-hour format with AM/PM
+    return dt.strftime('%Y-%m-%d %I:%M:%S %p')
 
 
 def get_player_data(player):
@@ -47,7 +61,6 @@ def get_player_data(player):
         player_url = (player_data.data[0]['player_link'])
         player_team = (player_data.data[0]['team']
                        [:-7].replace('-', ' ').title())
-        print(player_team)
     else:
         print(f"No data found for player: {player}")
         return None, None
@@ -57,22 +70,20 @@ def get_player_data(player):
     return player_team, game_log_url
 
 
-def get_todays_game_id(player_team):
-    today = datetime.now().date()
+def get_players_game_id(player_team):
+    today = datetime.now().date()  # Ensure today's date is correct
     games_data = supabase.table('games').select('*').execute()
 
     if not games_data.data:
         print("No games data found.")
         return None
 
-    todays_games = [game for game in games_data.data if convert_to_est(
-        game['commence_time']).date() == today]
-    print(f"Games from today: {todays_games}")
+    todays_games = [game for game in games_data.data if datetime.strptime(
+        game['commence_time'], '%Y-%m-%dT%H:%M:%S').date() == today]
 
-    game_ids = []
-    for game in todays_games:
-        if player_team in [game['team1'], game['team2']]:
-            game_ids.append(game['game_id'])
+    game_ids = [game['game_id'] for game in todays_games if player_team in [
+        game['team1'], game['team2']]]
+
     if not game_ids:
         print(f"No game found for {player_team} today.")
         return None
@@ -94,12 +105,10 @@ def get_player_prop_odds(player, prop, game_id, api_key):
         'triples': 'batter_triples',
         'walks': 'batter_walks',
         'strikeouts': 'batter_strikeouts',
-        'pitcher strikeouts': 'pitcher_strikeouts',
-        'pitcher record a win': 'pitcher_record_a_win',
-        'pitcher hits allowed': 'pitcher_hits_allowed',
-        'pitcher walks': 'pitcher_walks',
-        'pitcher earned runs': 'pitcher_earned_runs',
-        'pitcher outs': 'pitcher_outs'
+        'pitcher_strikeouts': 'pitcher_strikeouts',
+        'pitcher_hits_allowed': 'pitcher_hits_allowed',
+        'pitcher_walks': 'pitcher_walks',
+        'pitcher_earned_runs': 'pitcher_earned_runs'
     }
 
     market = market_identifiers.get(prop.lower())
@@ -173,8 +182,9 @@ def get_player_game_log(url, prop):
         return None, None
 
     headers = [th.get_text(strip=True) for th in thead]
+    print(f"Table Headers: {headers}")
 
-    # Dictionary to map props to table headers
+    # Dictionary to map props to table headers for both batters and pitchers
     prop_identifiers = {
         'hits': 'hhits',
         'runs': 'rruns',
@@ -183,7 +193,11 @@ def get_player_game_log(url, prop):
         'strikeouts': 'sostrikeouts',
         'walks': 'bbbaseonballs(walk)',
         'doubles': '2bdoubles',
-        'triples': '3btriples'
+        'triples': '3btriples',
+        'pitcher_hits_allowed': 'hhits',
+        'pitcher_earned_runs': 'erearnedruns',
+        'pitcher_walks': 'bbbaseonballs(walk)',
+        'pitcher_strikeouts': 'sostrikeouts'
     }
 
     # Normalize prop to match the table headers
@@ -192,9 +206,13 @@ def get_player_game_log(url, prop):
         print(f"Property '{prop}' not found in the prop identifiers.")
         return None, None
 
+    print(f"Header Name for '{prop}': {header_name}")
+
     # Normalize headers for comparison
     normalized_headers = [header.lower().replace(' ', '')
                           for header in headers]
+
+    print(f"Normalized Headers: {normalized_headers}")
 
     # Determine the index of the column that matches the header name
     prop_index = None
@@ -233,45 +251,125 @@ def get_player_game_log(url, prop):
 def plot_game_log_data(player_name, prop, dates, game_log_data):
     values = [float(data) for data in game_log_data]
 
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(12, 6))  # Increase the size of the plot
     plt.bar(dates, values, color='b')
 
     plt.title(f'{player_name} - {prop.title()
-                                 } Over Last {len(game_log_data)} Games')
-    plt.xlabel('Date')
-    plt.ylabel(prop.title())
-    plt.xticks(rotation=45)
-    plt.grid(True)
+                                 } Over Last {len(game_log_data)} Games', fontsize=16)
+    plt.xlabel('Date', fontsize=14)
+    plt.ylabel(prop.title(), fontsize=14)
+    plt.xticks(rotation=45, fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.grid(False)
     plt.tight_layout()
-    plt.show()
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    plt.close()
+    return buf
 
 
-# Example usage
-player_name = 'Nolan Schanuel'
-prop = 'hits'
+# # Example usage
+# player_name = 'Nolan Schanuel'
+# prop = 'hits'
 
 
-player_team, game_log_url = get_player_data(player_name)
-if player_team:
-    game_ids = get_todays_game_id(player_team)
-    if game_ids:
-        for id in game_ids:
-            prop_odds = get_player_prop_odds(
-                player_name, prop, id, api_key)
+# player_team, game_log_url = get_player_data(player_name)
+# if player_team:
+#     game_ids = get_todays_game_id(player_team)
+#     if game_ids:
+#         for id in game_ids:
+#             prop_odds = get_player_prop_odds(
+#                 player_name, prop, id, api_key)
 
-            if prop_odds:
-                print(f"Odds for player '{player_name}' in game {id}:")
-                for odds in prop_odds:
-                    print(f"{odds['bookmaker']}: {
-                          odds['price']} ({odds['point']})")
+#             if prop_odds:
+#                 print(f"Odds for player '{player_name}' in game {id}:")
+#                 for odds in prop_odds:
+#                     print(f"{odds['bookmaker']}: {
+#                           odds['price']} ({odds['point']})")
 
-        # Fetch game log data for the specified prop
-        dates, game_log_data = get_player_game_log(game_log_url, prop)
-        if game_log_data:
-            print(f"Game log data for '{player_name}': {game_log_data}")
-            # Plot the game log data
-            plot_game_log_data(player_name, prop, dates, game_log_data)
-        else:
-            print(f"No game log data found for '{player_name}'.")
+#         # Fetch game log data for the specified prop
+#         dates, game_log_data = get_player_game_log(game_log_url, prop)
+#         if game_log_data:
+#             print(f"Game log data for '{player_name}': {game_log_data}")
+#             # Plot the game log data
+#             plot_game_log_data(player_name, prop, dates, game_log_data)
+#         else:
+#             print(f"No game log data found for '{player_name}'.")
+#     else:
+#         print(f"No game found for {player_team} today.")
+
+# Group command for streak-related commands
+
+
+@bot.group()
+async def prop(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send('Invalid prop command. Use `mlb prop help` for more information.')
+
+
+@prop.command(name='finder')
+async def prop_finder(ctx, *, player_name_prop):
+    api_key = os.getenv('ODDS_API_KEY')
+    try:
+        input_ = player_name_prop.split()
+        player_name = ' '.join(input_[:-1])
+        prop = input_[-1]
+    except ValueError:
+        await ctx.send("Please provide the player's name followed by the prop.")
+        return
+
+    print(f"Player Name: {player_name}, Prop: {prop}")
+
+    player_team, game_log_url = get_player_data(player_name)
+
+    print(f"Player Team: {player_team}, Game Log URL: {game_log_url}")
+
+    if not player_team:
+        await ctx.send(f"No data found for player: {player_name}")
+        return
+
+    game_ids = get_players_game_id(player_team)
+
+    print(f"Game IDs: {game_ids}")
+
+    if not game_ids:
+        await ctx.send(f"No game found for {player_team} today.")
+        return
+
+    odds_message = f"Odds for {player_name.title()} {prop.title()}:\n"
+    odds_found = False  # Flag to check if any odds are found
+
+    for game_id in game_ids:
+        prop_odds = get_player_prop_odds(player_name, prop, game_id, api_key)
+
+        if prop_odds:
+            odds_found = True
+            for odds in prop_odds:
+                sign = '+' if odds['price'] > 0 else ''
+                odds_message += f"Over {odds['point']
+                                        } - [{sign}{odds['price']}]\n"
+
+    if not odds_found:
+        odds_message += f"No odds found for player '{
+            player_name}' in market '{prop}'."
+
+    await ctx.send(odds_message)
+
+    # Fetch game log data for the specified prop
+    dates, game_log_data = get_player_game_log(game_log_url, prop)
+    if game_log_data:
+        plot_buf = plot_game_log_data(player_name, prop, dates, game_log_data)
+
+        file = discord.File(fp=plot_buf, filename="plot.png")
+        embed = discord.Embed(
+            title=f"{player_name} - {prop.title()} Over Last {len(game_log_data)} Games")
+        embed.set_image(url="attachment://plot.png")
+
+        await ctx.send(file=file, embed=embed)
     else:
-        print(f"No game found for {player_team} today.")
+        await ctx.send(f"No game log data found for '{player_name}'.")
+
+# Run the bot with the token from the developer portal
+bot.run(os.getenv('BOT_TOKEN'))
